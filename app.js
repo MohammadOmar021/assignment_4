a3d06c1979431c859ca459c165ce51f12988405c
mohammadomar <umerbhatti021@gmail.com>
2024-12-26 16:57:06 +0500

    completed

---
+++ app.js
@@ -0,0 +1,106 @@
+// Array
+
+//Question 1
+
+// let array =[];
+
+//Question2
+// let studentNames = new Array();
+
+
+//Question3
+// let stringArray = [apples, mangoes, grapes]
+
+//Question4
+
+// let numArray = [1,2,3,4,5]
+
+//Question5
+// let boolArray = [0,1,0,1,0]
+
+//Question6
+// let mixedArray = ["apples", "mangoes", 5, 6, 7, 8, 0, 1, 0]
+// document.write("<h1>", mixedArray, "</h1>");
+
+
+//Question7
+
+// let availableEducation = ["phd", "Masters", "Bachelor"]
+// document.write("</br><h1>", availableEducation[0], "</h1>");
+// document.write("</br><h1>", availableEducation[1], "</h1>");
+// document.write("</br><h1>", availableEducation[2], "</h1>");
+
+//Question8
+// let studentNames=["Muhammad", "Umer", "hasan"]
+// let percentage=[50, 60, 70]
+// let marks=[150, 160, 170]
+// document.write("<h1> Score of ", studentNames[0], " is ",marks[0]," and he got percentage ", percentage[0], "%")
+// document.write("</br><h1> Score of ", studentNames[1], " is ",marks[1]," and he got percentage ", percentage[1], "%")
+// document.write("</br><h1> Score of ", studentNames[2], " is ",marks[2]," and he got percentage ", percentage[2], "%")
+
+
+//Question9
+
+// let colorName = ["violet", "indigo", "blue"]
+// document.write("<h1>", colorName, "</h1>")
+// let userInput = prompt("Enter the color name you want to add on the first")
+// colorName[0]=userInput
+// document.write("<h1>", colorName, "</h1>")
+// let userInput1 = prompt("Enter the color name you want to add on the last")
+// colorName[3]=userInput1
+// document.write("<h1>", colorName, "</h1>")
+
+
+// colorName.unshift("red")
+// colorName.unshift("yellow")
+// document.write("<h1>", colorName, "</h1>")
+
+// colorName.pop();
+// document.write("<h1>", colorName, "</h1>")
+
+// let index = prompt("Enter the index you want to add color")
+// let colorName1 = prompt("Enter color name you wnat to add")
+
+// colorName[index]=colorName1
+// document.write("<h1>", colorName, "</h1>")
+
+// colorName.splice(3, 2)
+// document.write("<h1>", colorName, "</h1>")
+
+//Question10
+
+// let scores = [250, 350, 270, 150]
+// document.write("<h1>", scores, "</h1>")
+
+// scores.sort();
+// document.write("<h1> After sorting ", scores, "</h1>")
+
+//Question 11
+
+// let citiesName = ["Karachi", "Hyderabad", "Lahore", "Multan"]
+// document.write("<h1>", citiesName, "</h1>")
+// let selectedCities = [citiesName[1], citiesName[3]]
+// document.write("<h1> Selected cities are ", selectedCities, "</h1>")
+
+
+// Question 12
+// let arr = ["this", "is", "my", "cat"]
+// document.write("<h1>", arr, "</h1>")
+
+// document.write("<h1> After joining ", arr.join(" "), "</h1>")
+
+
+//Question 13
+
+// let parts = ["keyboard", "mouse", "cpu", "speakers"]
+// document.write("<h1>", parts, "</h1>")
+// let lastItem=parts.shift()
+// document.write("<h1> Last element was ", lastItem, "</h1>")
+// document.write("<h1> After removing last element ", parts, "</h1>")
+
+
+// Question14
+// let parts = ["keyboard", "Mouse", "Speakers", "Cpu"]
+// document.write("<h1>", parts, "</h1>")
+// parts.pop();
+// document.write("<h1> After removing last element ", parts, "</h1>")
